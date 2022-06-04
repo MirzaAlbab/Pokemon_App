e04c2a0454d63488746156a56cf2b6ba82aa65b8
unknown <mirzaalbabfirdaus17@gmail.com>
2022-06-05 00:20:23 +0700

    add stack navigation

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import CodePush from 'react-native-code-push';
+import React from 'react';
+import Root from './src/routes/Root';
 
 const CodePushOptions = {
   checkFrequency: CodePush.CheckFrequency.ON_APP_START,
+};
+const App = () => {
+  return <Root />;
 };
 export default CodePush(CodePushOptions)(App);
