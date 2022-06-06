28527f300f21fa2f7f77a711393f4d5df2b296ed
unknown <mirzaalbabfirdaus17@gmail.com>
2022-06-06 15:52:17 +0700

    add animation

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import CodePush from 'react-native-code-push';
 import React from 'react';
 import Root from './src/routes/Root';
+import {NavigationContainer} from '@react-navigation/native';
 
 const CodePushOptions = {
   checkFrequency: CodePush.CheckFrequency.ON_APP_START,
 };
 const App = () => {
-  return <Root />;
+  return (
+    <NavigationContainer>
+      <Root />
+    </NavigationContainer>
+  );
 };
 export default CodePush(CodePushOptions)(App);
